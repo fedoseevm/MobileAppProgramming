f8bbe5e7602963d2fb1eb4dbaf0d361467f6101c
fedoseevm <maksim.fedoseev122@gmail.com>
2025-10-08 14:53:29 +0200

    Add SpokojnySklep project

---
+++ SpokojnySklep/src/Product.java
@@ -0,0 +1,11 @@
+public abstract class Product {
+    private String Name;
+    private int Price;
+
+    public String getName() {
+        return Name;
+    }
+    public int getPrice() {
+        return Price;
+    }
+}
