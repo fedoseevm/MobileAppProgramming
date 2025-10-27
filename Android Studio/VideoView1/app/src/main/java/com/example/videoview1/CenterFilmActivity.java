9e55a9a56e8b74e2bd05f960db28fcef61109c69
fedoseevm <maksim.fedoseev122@gmail.com>
2025-10-27 08:49:39 +0100

    Add VideoView1

---
+++ Android Studio/VideoView1/app/src/main/java/com/example/videoview1/CenterFilmActivity.java
@@ -0,0 +1,45 @@
+package com.example.videoview1;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CenterFilmActivity extends AppCompatActivity {
+    VideoView videoView;
+    final String videoURL = "https://cdn.pixabay.com/video/2025/07/27/293788_small.mp4";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_full_film);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        videoView = findViewById(R.id.video1);
+        Uri uri = Uri.parse(videoURL);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+        videoView.setOnPreparedListener(mp -> videoView.start());
+
+        videoView.setOnErrorListener((mp, what, extra) -> {
+            System.out.println("Video playback error: what=" + what + ", extra=" + extra);
+            Toast.makeText(CenterFilmActivity.this, "Video playback error: what=" + what + ", extra=" + extra, Toast.LENGTH_SHORT).show();
+            return true;
+        });
+    }
+}
