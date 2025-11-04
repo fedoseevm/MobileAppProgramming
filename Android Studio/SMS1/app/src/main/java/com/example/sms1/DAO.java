c8be11f2acdbc440cb02ea20a38cda5c34eb75e3
fedoseevm <maksim.fedoseev122@gmail.com>
2025-11-04 10:29:44 +0100

    Modify DAO

--- Android Studio/SMS1/app/src/main/java/com/example/sms1/DAO.java
+++ Android Studio/SMS1/app/src/main/java/com/example/sms1/DAO.java
@@ -1,7 +1,11 @@
 package com.example.sms1;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DAO {
     private DbHelper dbHelper;
@@ -10,7 +14,6 @@ public class DAO {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         ContentValues cv = new ContentValues();
-        cv.put("COL_id", "null");
         cv.put("COL_TITLE", sms.getTitle());
         cv.put("COL_MESSAGE", sms.getTitle());
 
@@ -18,5 +21,27 @@ public class DAO {
 
         db.close();
         return id;
+    }
+
+    public List<SMS> getSMSList() {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        String query = "SELECT * FROM sms;";
+        Cursor cursor = db.rawQuery(query, null);   // Wynik zapytania do bazy danych
+        int id, idIndex, titleIndex, messageIndex;
+        String title, message;
+
+        List<SMS> listSMS = new ArrayList<SMS>();
+        while (cursor.moveToNext()) {
+            idIndex = cursor.getColumnIndex("_id");
+            titleIndex = cursor.getColumnIndex("title");
+            messageIndex = cursor.getColumnIndex("message");
+            id = cursor.getInt(idIndex);
+            title = cursor.getString(titleIndex);
+            message = cursor.getString(messageIndex);
+            listSMS.add(new SMS(id, title, message));
+        }
+        cursor.close();
+        db.close();
+        return listSMS;
     }
 }
