6b2eeaa8024fb5ec1f690cd06eadab9027731a61
fedoseevm <maksim.fedoseev122@gmail.com>
2025-11-05 14:10:32 +0100

    Add listView variable declaration

--- Android Studio/SMS1/app/src/main/java/com/example/sms1/MainActivity.java
+++ Android Studio/SMS1/app/src/main/java/com/example/sms1/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBar;
@@ -15,6 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton addBtn;
+    ListView listView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,5 +39,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent1);
             }
         });
+
+        listView1 = findViewById(R.id.listView1);
     }
 }
