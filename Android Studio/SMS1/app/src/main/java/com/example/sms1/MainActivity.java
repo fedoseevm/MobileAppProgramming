df6566bfaf285afa65b26d74d679f70b510001a7
fedoseevm <maksim.fedoseev122@gmail.com>
2025-11-05 13:46:47 +0100

    Add Button functionality

--- Android Studio/SMS1/app/src/main/java/com/example/sms1/MainActivity.java
+++ Android Studio/SMS1/app/src/main/java/com/example/sms1/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.sms1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBar;
@@ -10,7 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton addBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Your SMS's");
+
+        addBtn = findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, AddSMSActivity.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
