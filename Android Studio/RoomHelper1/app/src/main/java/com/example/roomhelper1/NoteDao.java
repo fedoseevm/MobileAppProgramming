cf52db2af47b859f638e74e92715f8439581da93
fedoseevm <maksim.fedoseev122@gmail.com>
2025-11-06 14:09:42 +0100

    Change NoteDao

--- Android Studio/RoomHelper1/app/src/main/java/com/example/roomhelper1/NoteDao.java
+++ Android Studio/RoomHelper1/app/src/main/java/com/example/roomhelper1/NoteDao.java
@@ -2,16 +2,23 @@ package com.example.roomhelper1;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
 @Dao
 public interface NoteDao {
-    @Query("SELECT * FROM Note;")
+    @Insert
+    void addNote(Note note);
+
+    @Query("SELECT * FROM notes;")
     List<Note> getAllNotes();
 
-    @Insert
-    void InsertNote(Note note);
+    @Delete
+    void removeNote(Note note);
+    @Update
+    void updateNote(Note note);
 }
