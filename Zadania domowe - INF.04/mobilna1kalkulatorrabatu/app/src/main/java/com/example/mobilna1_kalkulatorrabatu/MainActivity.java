2187736eab58bd030fc04fb73ad7baec416a2ac8
fedoseevm <maksim.fedoseev122@gmail.com>
2026-01-22 14:28:19 +0100

    Edit kalkulator rabatu

--- Zadania domowe - INF.04/mobilna1kalkulatorrabatu/app/src/main/java/com/example/mobilna1_kalkulatorrabatu/MainActivity.java
+++ Zadania domowe - INF.04/mobilna1kalkulatorrabatu/app/src/main/java/com/example/mobilna1_kalkulatorrabatu/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        rgRodzajRabatu = findViewById(R.id.rg_rodzajRabatu);
+        rgRodzajRabatu.check(R.id.rb1);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -44,20 +46,63 @@ public class MainActivity extends AppCompatActivity {
         btnOblicz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textRabat.setText("Rabat: fsdf");
+                textRabat = findViewById(R.id.tv_rabat);
+                textRabat.setText("Rabat: ");
 
-//                etCena = findViewById(R.id.et_cena);
-//                String inputCena = etCena.getText().toString().trim();
-//                try {
-//                    int cena = Integer.parseInt(inputCena);
-//                    if (cena < 10 || cena > 10000) {
-//                        throw new NumberFormatException();
-//                    }
-//                }
-//                catch (NumberFormatException e) {
-//                    Toast.makeText(MainActivity.this, "Cena musi być liczbą dodatnią (10-10000 zł)", Toast.LENGTH_SHORT).show();
-//                    return;
-//                }
+                etCena = findViewById(R.id.et_cena);
+                String inputCena = etCena.getText().toString().trim();
+                int cena;
+                try {
+                    cena = Integer.parseInt(inputCena);
+                    if (cena < 10 || cena > 10000) {
+                        throw new NumberFormatException();
+                    }
+                }
+                catch (NumberFormatException e) {
+                    Toast.makeText(MainActivity.this, "Cena musi być liczbą dodatnią (10-10000 zł)", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                seekBarProcentRabatu = findViewById(R.id.seekBar_procentRabatu);
+                int procentRabatu = seekBarProcentRabatu.getProgress();
+
+                switch (rgRodzajRabatu.getCheckedRadioButtonId()) {
+                    case 1:
+                        procentRabatu += 5;
+                        break;
+                    case 2:
+                        procentRabatu += 10;
+                        break;
+                }
+
+                cbCzyKartaKlienta = findViewById(R.id.cb_czyKarta);
+                if (cbCzyKartaKlienta.isChecked()) {
+                    procentRabatu += 5;
+                }
+
+//                boolean czyDarmowaWysylka = findViewById(R.id.switch_wysylka).createAccessibilityNodeInfo().isChecked();
+                switchCzyDarmowaWysylka = findViewById(R.id.switch_wysylka);
+                boolean czyDarmowaWysylka = switchCzyDarmowaWysylka.isChecked();
+
+                textRabat.setText("Rabat: " + procentRabatu + "%");
+
+                imageOszednosci = findViewById(R.id.iv_oszczednosci);
+                if (procentRabatu <= 20) {
+                    imageOszednosci.setImageResource(R.drawable.oszednosci_male);
+                }
+                else if (procentRabatu <= 40) {
+                    imageOszednosci.setImageResource(R.drawable.oszednosci_srednie);
+                }
+                else {
+                    imageOszednosci.setImageResource(R.drawable.oszednosci_duze);
+                }
+
+                textWynik = findViewById(R.id.tv_wynik);
+                textWynik.setText(
+                        "Cena przed: " + cena + "zł\n" +
+                        "Rabat: " + procentRabatu + "%\n" +
+                        "Cena końcowa: " + cena * (1 - procentRabatu / 100) + "zł\n" +
+                        "Wysyłka: ");
             }
         });
     }
