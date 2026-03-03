13726cd538dc3810b3a3fad4eb653e60bce7a684
fedoseevm <maksim.fedoseev122@gmail.com>
2026-03-04 00:33:18 +0100

    Complete generujZaawansowaneHaslo method, finish the project

--- Zadania domowe - INF.04/mobilna2GeneratorHasel/app/src/main/java/com/example/mobilna2_generatorhasel/MainActivity.java
+++ Zadania domowe - INF.04/mobilna2GeneratorHasel/app/src/main/java/com/example/mobilna2_generatorhasel/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,8 +22,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     TextView textDlugoscHasla, textWynik;
@@ -72,35 +74,105 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Minimum 1 Checkbox musi być zaznaczony!", LENGTH_SHORT).show();
                 }
                 else {
+                    int zlozonoscHasla = 0;
+
                     seekBarIloscZnakow = findViewById(R.id.seekBar_ilosc_znakow);
                     final int iloscHasel = Integer.parseInt(spinnerLiczbaHasel.getSelectedItem().toString());
                     final int iloscZnakow = seekBarIloscZnakow.getProgress();
+                    zlozonoscHasla += iloscZnakow;
+
                     String pulaZnakow = "";
                     if (cbCyfry.isChecked()) {
                         pulaZnakow += "0123456789";
+                        zlozonoscHasla += 10;
                     }
                     if (cbMaleLitery.isChecked()) {
                         pulaZnakow += "qwertyuiopasdfghjklzxcvbnm";
+                        zlozonoscHasla += 10;
                     }
                     if (cbWielkieLitery.isChecked()) {
                         pulaZnakow += "QWERTYUIOPASDFGHJKLZXCVBNM";
+                        zlozonoscHasla += 15;
                     }
                     if (cbZnakiSpecjalne.isChecked()) {
-                        pulaZnakow += "!@#$%^&&*";
+                        pulaZnakow += "!@#$%^&*";
+                        zlozonoscHasla += 20;
                     }
 
-                    String wynik = "";
+                    ivSilaHasla = findViewById(R.id.iv_sila_hasla);
+                    if (zlozonoscHasla < 30) {
+                        ivSilaHasla.setImageResource(R.drawable.slabe);
+                    }
+                    else if (zlozonoscHasla < 50) {
+                        ivSilaHasla.setImageResource(R.drawable.srednie);
+                    }
+                    else {
+                        ivSilaHasla.setImageResource(R.drawable.mocne);
+                    }
+
+                    textWynik = findViewById(R.id.tv_wynik);
+                    String wynik = "Siła hasła: " + zlozonoscHasla + "\n";
                     if (rgTryb.getCheckedRadioButtonId() != R.id.rb_zaawansowany) {
-                        for (var i = 0; i < iloscZnakow; i++) {
-                            wynik += generujHaslo(pulaZnakow);
+                        for (var i = 0; i < iloscHasel; i++) {
+                            wynik += generujHaslo(pulaZnakow, iloscZnakow, new Random()) + "\n";
                         }
+                    }
+                    else {
+                        for (var i = 0; i < iloscHasel; i++) {
+                            wynik += generujZaawansowaneHaslo(pulaZnakow, iloscZnakow, new Random()) + "\n";
+                        }
+                    }
+                    textWynik.setText(wynik);
+
+                    switchUstawienia = findViewById(R.id.switch_ustawienia);
+                    if (!switchUstawienia.isChecked()) {
+                        seekBarIloscZnakow.setProgress(12);
+
+                        cbMaleLitery.setChecked(false);
+                        cbWielkieLitery.setChecked(false);
+                        cbCyfry.setChecked(false);
+                        cbZnakiSpecjalne.setChecked(false);
+
+                        spinnerLiczbaHasel.setSelection(0);
+
+                        rgTryb.clearCheck();
                     }
                 }
             }
 
-            private String generujHaslo(String pulaZnakow) {
+            private String generujZaawansowaneHaslo(String pulaZnakow, int iloscZnakow, Random random) {
+                String preHaslo = "";
                 String haslo = "";
-                // Do zrobienia
+
+                preHaslo += pulaZnakow.charAt(random.nextInt(10))
+                        + pulaZnakow.charAt(random.nextInt(26) + 10)
+                        + pulaZnakow.charAt(random.nextInt(26) + 36)
+                        + pulaZnakow.charAt(random.nextInt(8) + 62);
+
+                for (var i = 0; i < iloscZnakow - 4; i++) {
+                    preHaslo += pulaZnakow.charAt(random.nextInt(pulaZnakow.length()));
+                }
+
+                List<Character> listaZnakowHasla = new ArrayList<>();
+
+                for (var i = 0; i < preHaslo.length(); i++) {
+                    listaZnakowHasla.add(preHaslo.charAt(i));
+                }
+
+                Collections.shuffle(listaZnakowHasla);
+
+                for (var i = 0; i < listaZnakowHasla.size(); i++) {
+                    haslo += listaZnakowHasla.get(i);
+                }
+
+                return haslo;
+            }
+
+            private String generujHaslo(String pulaZnakow, int iloscZnakow, Random random) {
+                String haslo = "";
+                for (var i = 0; i < iloscZnakow; i++) {
+                    haslo += pulaZnakow.charAt(random.nextInt(pulaZnakow.length()));
+                }
                 return haslo;
             }
         });
